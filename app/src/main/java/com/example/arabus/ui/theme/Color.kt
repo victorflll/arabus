2204c0015965d9e937835d356c7699d8622566ec
Victor <victorluan.lobo@gmail.com>
2025-01-09 19:04:05 -0300

    feat: add AppTextField and set AppColors

--- app/src/main/java/com/example/arabus/ui/theme/Color.kt
+++ app/src/main/java/com/example/arabus/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.arabus.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val AppGreen = Color(0xFF3D8499)
+val AppGreenOpacity = AppGreen.copy(alpha = 0.2f)
+val AppWhite = Color.White
+val AppBlack = Color.Black
+val AppRed = Color.Red
+val AppGrey = Color.Black.copy(alpha = 0.5f)
