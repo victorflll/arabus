5767f257209c94a8bb3dc4a04a9e18d724b1a1e9
samila.oliveira <sro3@aluno.ifal.edu.br>
2025-01-16 01:40:43 -0300

    fixing main file

--- app/src/main/java/com/example/arabus/MainActivity.kt
+++ app/src/main/java/com/example/arabus/MainActivity.kt
@@ -1,59 +1,47 @@
 package com.example.arabus
 
-package com.example.arabus
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.arabus.ui.HomeScreenPath
-import com.example.arabus.ui.SearchRouteScreenPath
-import com.example.arabus.ui.ViewRouteScreenPath
-import com.example.arabus.ui.components.AppButton
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.arabus.ui.theme.ArabusTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            App()
+            ArabusTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Arabus",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
-private fun App() {
-    val navController = rememberNavController()
-
-    NavHost(
-        navController = navController,
-        startDestination = HomeScreenPath
-    ) {
-        composable(HomeScreenPath) { HomeScreen(navController) }
-        composable(SearchRouteScreenPath) { SearchRouteScreen(navController) }
-        composable(ViewRouteScreenPath) { ViewRouteScreen(navController) }
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
+@Preview(showBackground = true)
 @Composable
-fun HomeScreen(navController: NavHostController) {
-    Scaffold { innerPadding ->
-        Surface(
-            modifier = Modifier
-                .padding(innerPadding)
-        ) {
-            AppButton("Go to Search routes", onClick = {
-                navController.navigate(SearchRouteScreenPath)
-            })
-        }
+fun GreetingPreview() {
+    ArabusTheme {
+        Greeting("Arabus")
     }
 }
