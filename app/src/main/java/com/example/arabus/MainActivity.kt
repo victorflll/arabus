a2e941be15d88d6bb79103d79307b4ea4b98b76a
Guilherme <guylhermee.b@hotmail.com>
2025-01-19 17:11:34 -0300

    feat: update MainActivity to incorporate HomeScreen functionality

--- app/src/main/java/com/example/arabus/MainActivity.kt
+++ app/src/main/java/com/example/arabus/MainActivity.kt
@@ -3,25 +3,36 @@ package com.example.arabus
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.arabus.components.AppScaffold
+import com.example.arabus.ui.HistoryScreenPath
 import com.example.arabus.ui.HomeScreenPath
 import com.example.arabus.ui.SearchRouteScreenPath
 import com.example.arabus.ui.ViewRouteScreenPath
-import com.example.arabus.ui.components.AppButton
+import com.example.arabus.ui.components.AppTextField
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             App()
         }
@@ -31,7 +42,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun App() {
     val navController = rememberNavController()
-
     NavHost(
         navController = navController,
         startDestination = HomeScreenPath
@@ -39,19 +49,50 @@ private fun App() {
         composable(HomeScreenPath) { HomeScreen(navController) }
         composable(SearchRouteScreenPath) { SearchRouteScreen(navController) }
         composable(ViewRouteScreenPath) { ViewRouteScreen(navController) }
+        composable(HistoryScreenPath) { HistoryScreen(navController) }
     }
 }
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    Scaffold { innerPadding ->
-        Surface(
-            modifier = Modifier
-                .padding(innerPadding)
-        ) {
-            AppButton("Go to Search routes", onClick = {
-                navController.navigate(SearchRouteScreenPath)
-            })
+    AppScaffold(navController = navController) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            MapView()
+            AppTextField(
+                placeholder = "Para onde vamos hoje?",
+                textState = "",
+                onValueChange = {},
+                modifier = Modifier.padding(32.dp),
+                trailingIcon = {
+                    IconButton(onClick = { navController.navigate(SearchRouteScreenPath) }) {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Buscar")
+                    }
+                }
+            )
         }
+    }
+}
+
+@Composable
+fun MapView() {
+    val initialPosition = LatLng(-9.754, -36.659)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(initialPosition, 14f)
+    }
+
+    var markerPosition by remember { mutableStateOf(initialPosition) }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+        onMapClick = { latLng ->
+            markerPosition = latLng
+        }
+    ) {
+        Marker(
+            state = MarkerState(position = markerPosition),
+            title = "Ponto Selecionado",
+            snippet = "Novo Local"
+        )
     }
 }
