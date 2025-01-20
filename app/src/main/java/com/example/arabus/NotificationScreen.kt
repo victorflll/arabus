02afedf8faedc37feb2c966bc5b4c3138ceaed17
samila.oliveira <sro3@aluno.ifal.edu.br>
2025-01-20 16:00:53 -0300

    Edit main activity and bottom navigation bar

--- app/src/main/java/com/example/arabus/NotificationScreen.kt
+++ app/src/main/java/com/example/arabus/NotificationScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +16,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.example.arabus.ui.theme.AppGreenOpacity
 import com.example.arabus.ui.theme.ArabusTheme
 import com.example.arabus.ui.theme.TypographyColor
