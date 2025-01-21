abf2a83df0281c761000984d6a042351f1ef8946
Victor <victorluan.lobo@gmail.com>
2025-01-21 01:08:23 -0300

    fix conflicts

--- app/src/main/java/com/example/arabus/ui/MainActivity.kt
+++ app/src/main/java/com/example/arabus/ui/MainActivity.kt
@@ -1,4 +1,5 @@
-package com.example.arabus
+package com.example.arabus.ui
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,7 +10,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
@@ -18,6 +24,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.arabus.FavoritesScreenPath
 import com.example.arabus.HistoryScreenPath
 import com.example.arabus.HomeScreenPath
 import com.example.arabus.LoginRouteScreen
@@ -26,13 +33,13 @@ import com.example.arabus.SearchRouteScreenPath
 import com.example.arabus.ViewRouteScreenPath
 import com.example.arabus.components.AppScaffold
 import com.example.arabus.ui.components.AppTextField
+import com.example.arabus.ui.view.RouteViewModel
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
-import com.example.arabus.ui.view.RouteViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -101,7 +108,8 @@ fun MapView(textState: MutableState<String>) {
         cameraPositionState = cameraPositionState,
         onMapClick = { latLng ->
             markerPosition = latLng
-            textState.value = "Lat: ${latLng.latitude}, Lng: ${latLng.longitude}"  // Preenche o input com a coordenada
+            textState.value =
+                "Lat: ${latLng.latitude}, Lng: ${latLng.longitude}"  // Preenche o input com a coordenada
         }
     ) {
         markerPosition?.let {
