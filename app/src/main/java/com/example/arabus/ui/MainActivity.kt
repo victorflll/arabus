fbe3cbdcd291cb38d90dd4436dc9f60516e814c1
Guilherme <guylhermee.b@hotmail.com>
2025-01-22 00:52:57 -0300

    fix: move HomeScreen component to a new page and change the button icon to access the history

--- app/src/main/java/com/example/arabus/ui/MainActivity.kt
+++ app/src/main/java/com/example/arabus/ui/MainActivity.kt
@@ -3,24 +3,9 @@ package com.example.arabus.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -34,17 +19,9 @@ import com.example.arabus.NotificationScreenPath
 import com.example.arabus.RegisterRouteScreen
 import com.example.arabus.SearchRouteScreenPath
 import com.example.arabus.ViewRouteScreenPath
-import com.example.arabus.components.AppScaffold
-import com.example.arabus.ui.components.AppTextField
-import com.example.arabus.ui.utils.Permissions
+import com.example.arabus.ui.screens.HomeScreen
 import com.example.arabus.ui.view.HistoryViewModel
 import com.example.arabus.ui.view.RouteViewModel
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -88,59 +65,5 @@ private fun App() {
         composable(NotificationScreenPath) { NotificationScreen(navController) }
         composable(LoginRouteScreen) { ViewLoginScreen(navController) }
         composable(RegisterRouteScreen) { ViewRegisterScreen(navController) }
-    }
-}
-
-@Composable
-fun HomeScreen(navController: NavHostController) {
-    val textState = remember { mutableStateOf("") }
-    AppScaffold(navController = navController) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            MapView(textState)
-            AppTextField(
-                placeholder = "Para onde vamos hoje?",
-                textState = textState.value,
-                onValueChange = { textState.value = it },
-                modifier = Modifier.padding(32.dp),
-                trailingIcon = {
-                    IconButton(onClick = { navController.navigate(SearchRouteScreenPath) }) {
-                        Icon(imageVector = Icons.Default.Search, contentDescription = "Buscar")
-                    }
-                }
-            )
-        }
-    }
-}
-
-@Composable
-private fun BuildBody(textState: MutableState<String>) {
-    Permissions.RequestLocationPermission { MapView(textState) }
-}
-
-@Composable
-fun MapView(textState: MutableState<String>) {
-    val initialPosition = LatLng(-9.754, -36.659)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(initialPosition, 14f)
-    }
-
-    var markerPosition by remember { mutableStateOf<LatLng?>(null) }
-
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-        onMapClick = { latLng ->
-            markerPosition = latLng
-            textState.value =
-                "Lat: ${latLng.latitude}, Lng: ${latLng.longitude}"  // Preenche o input com a coordenada
-        }
-    ) {
-        markerPosition?.let {
-            Marker(
-                state = MarkerState(position = it),
-                title = "Ponto Selecionado",
-                snippet = "Lat: ${it.latitude}, Lng: ${it.longitude}"
-            )
-        }
     }
 }
